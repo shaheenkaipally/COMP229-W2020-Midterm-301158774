b52baceba142e0c4fb63d9aef4be262cb998c160
shaheenkaipally <shaheenkm3@outlook.com>
2021-03-05 15:47:14 +0530

    first commit

---
+++ server/config/db.js
@@ -0,0 +1,9 @@
+/**
+ * Add your code here to fix URI connection
+ * 
+ */
+module.exports = {
+    "URI": "mongodb+srv://shaheen:shaheen@cluster0.drb6j.mongodb.net/bookstore?retryWrites=true&w=majority"
+
+
+}
